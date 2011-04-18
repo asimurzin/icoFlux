32e051bb842bd1a7e21af2ba259e5109b27eeb8c
Andrey Simurzin <asimurzin@gmail.com>
2011-04-18 11:10:55 +0400

    to simplify conditions and add entry_point

--- icoFlux/__init__.py
+++ icoFlux/__init__.py
@@ -24,95 +24,52 @@
 
 
 #---------------------------------------------------------------------------
-import sys, os
 from Foam import FOAM_VERSION,  FOAM_BRANCH_VERSION, FOAM_REF_VERSION
 if FOAM_VERSION( "<=", "010401" ):
-   if __name__ == "__main__" :
-      argv = sys.argv
-      if len( argv ) > 1 and argv[ 1 ] == "-test":
-         argv = None
-         test_dir= os.path.join( os.environ[ "PYFOAM_TESTING_DIR" ],'cases', 'local', 'r1.4.1-dev', 'icoFoam' )
-         argv = [ __file__, test_dir, 'cavity' ]
-         pass
-      from Foam.applications.solvers.incompressible.r1_4_1_dev.icoFoam import main_embedded
-      os._exit( main_embedded( len( argv ), argv ) )
-   else:
-      from Foam.applications.solvers.incompressible.r1_4_1_dev.icoFoam import *
-      pass
-   pass
-pass
+    from icoFlux.r1_4_1_dev import *
+    pass
 
 
 #-------------------------------------------------------------------------------------------------
 if FOAM_REF_VERSION( "==", "010500" ):
-   if __name__ == "__main__" :
-      argv = sys.argv
-      if len( argv ) > 1 and argv[ 1 ] == "-test":
-         argv = None
-         test_dir= os.path.join( os.environ[ "PYFOAM_TESTING_DIR" ],'cases', 'local', 'r1.5', 'icoFoam', 'cavity' )
-         argv = [ __file__, "-case", test_dir ]
-         pass
-      from Foam.applications.solvers.incompressible.r1_5.icoFoam import main_standalone
-      os._exit( main_standalone( len( argv ), argv ) )
-      pass
-   else:
-      from Foam.applications.solvers.incompressible.r1_5.icoFoam import *
-      pass
-   pass
+    from icoFlux.r1_5 import *
+    pass
 
 
 #-------------------------------------------------------------------------------------------------
 if FOAM_BRANCH_VERSION( "dev", "==", "010500" ):
-   if __name__ == "__main__" :
-      argv = sys.argv
-      if len( argv ) > 1 and argv[ 1 ] == "-test":
-         argv = None
-         test_dir= os.path.join( os.environ[ "PYFOAM_TESTING_DIR" ],'cases', 'propogated', 'r1.5-dev', 'icoFoam', 'cavity' )
-         argv = [ __file__, "-case", test_dir ]
-         pass
-      from Foam.applications.solvers.incompressible.r1_5_dev.icoFoam import main_standalone
-      os._exit( main_standalone( len( argv ), argv ) )
-      pass
-   else:
-      from Foam.applications.solvers.incompressible.r1_5_dev.icoFoam import *
-      pass
-   pass
+    from icoFlux.r1_5_dev import *
+    pass
 
 
 #-------------------------------------------------------------------------------------------------
 if FOAM_REF_VERSION( ">=", "010600" ):
-   if __name__ == "__main__" :
-      argv = sys.argv
-      if len( argv ) > 1 and argv[ 1 ] == "-test":
-         argv = None
-         test_dir= os.path.join( os.environ[ "PYFOAM_TESTING_DIR" ],'cases', 'propogated', 'r1.6', 'incompressible', 'icoFoam', 'cavity' )
-         argv = [ __file__, "-case", test_dir ]
-         pass
-      from Foam.applications.solvers.incompressible.r1_6.icoFoam import main_standalone
-      os._exit( main_standalone( len( argv ), argv ) )
-      pass
-   else:
-      from Foam.applications.solvers.incompressible.r1_6.icoFoam import *
-      pass
-   pass
+    from icoFlux.r1_6 import *
+    pass
 
     
 #--------------------------------------------------------------------------------------
 if FOAM_BRANCH_VERSION( "dev", ">=", "010600" ):
-   if __name__ == "__main__" :
-      argv = sys.argv
-      if len( argv ) > 1 and argv[ 1 ] == "-test":
-         argv = None
-         test_dir= os.path.join( os.environ[ "PYFOAM_TESTING_DIR" ],'cases', 'propogated', 'r1.6', 'incompressible', 'icoFoam', 'cavity' )
-         argv = [ __file__, "-case", test_dir ]
-         pass
-      from Foam.applications.solvers.incompressible.r1_6_dev.icoFoam import main_standalone
-      os._exit( main_standalone( len( argv ), argv ) )
-      pass
-   else:
-      from Foam.applications.solvers.incompressible.r1_6_dev.icoFoam import *
-      pass
-   pass
+    from icoFlux.r1_6_dev import *
+    pass
 
+    
+#--------------------------------------------------------------------------------------
+def entry_point():
+    try:
+       import sys; argv = sys.argv
+       return main_standalone( len( argv ), argv )
+    except NameError:
+       print
+       print "There is no implementation of the current OpenFOAM version"
+       print
+       pass
+
+
+#--------------------------------------------------------------------------------------
+if __name__ == "__main__" :
+    entry_point()
+    pass
+    
     
 #--------------------------------------------------------------------------------------
